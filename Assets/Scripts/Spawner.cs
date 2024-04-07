68d8ac48eab1025b908296e5b0ab8274b6d5aa8e
Anthony Krueger <103562710@student.swin.edu.au>
2024-04-08 02:39:42 +1000

    Saved the arrays for the block types.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,6 +7,8 @@ public class Spawner : MonoBehaviour
     //Gets player object
     public GameObject fauxPlayer;
 
+
+    //Stores the blocks in their own seperate arrays
     public GameObject[] iceBlocks;
     public GameObject[] bounceBlocks;
 
@@ -14,7 +16,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-              
+      
     }
 
     // Update is called once per frame
