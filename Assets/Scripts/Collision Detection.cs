8ad8c3fe15c4d3b18cbb4a5aa3ec50e2f20c022b
Anthony Krueger <103562710@student.swin.edu.au>
2024-04-17 22:37:59 +1000

    Spawner and Arduino should be talking and work as intended. Needs to be tested with the Griddy.

--- Assets/Scripts/Collision Detection.cs
+++ Assets/Scripts/Collision Detection.cs
@@ -7,16 +7,7 @@ public class CollisionDetection : MonoBehaviour
     public bool hasJoint = false;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnCollisionEnter(Collision collision)
     {
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,24 +5,13 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using TMPro;
 using System;
+using UnityEditor.Search;
 
 public class Spawner : MonoBehaviour
 {
-    //Gets player object
-    public GameObject player;
-    public GameObject gameController;
+
     public Camera Camera;
-/*    public GameObject displayCamera;*/
 
-
-/*    public UnityEngine.UI.Button normalButton;
-    public UnityEngine.UI.Button icyButton;
-    public UnityEngine.UI.Button bouncyButton;
-
-    //Stores the blocks in their own seperate arrays
-    public GameObject[] normalBlocks;
-    public GameObject[] iceBlocks;
-    public GameObject[] bounceBlocks;*/
 
     public GameObject[] blocks;
 
@@ -33,39 +22,19 @@ public class Spawner : MonoBehaviour
     public bool pos2;
     public bool pos3;
 
-    public bool hasJoint = false;
+    private bool hasSpawned;
+    private bool buttonPressed;
 
     public TMP_Text blockType;
 
-/*    private bool isDisplayActive = false;*/
 
-
-
-    //Arduino will dictate which blocks spawn, implement TAB to change block properties.
-    //If blocks are spawned nect to each other, add a joint.
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-/*    public void OnClick(UnityEngine.UI.Button normalButton, UnityEngine.UI.Button icyButton, UnityEngine.UI.Button bouncyButton)
-    {
-        //Buttons to change the tag of the Game Controller.
-    }*/
-
-
-    // Update is called once per frame
     void Update()
-    {
-        Spawn();
-
+    {   
         CycleBlocks();
-
         DisplayPos();
+        Spawn();    
+
+        
     }
 
 
@@ -85,51 +54,42 @@ public class Spawner : MonoBehaviour
         {
             blockType.text = "Block Type: Bouncy";
         }
-        else
-        {
-            Debug.Log("Error");
-        }
     }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.GetComponent<Rigidbody>().CompareTag("Spocks") && !hasJoint) 
-        {
-            gameObject.AddComponent<FixedJoint>();
-            gameObject.GetComponent<FixedJoint>().connectedBody=collision.rigidbody;
-            hasJoint = true;
-        }
-    }
-
 
     void Spawn()
     {
-
-            if (Input.GetMouseButtonDown(0))
+            char[] input = GetComponent<ArduinoReader>().OutputArray;
+        if (input[0].ToString() == "1")
+        {
+            buttonPressed = true;
+            if (input[1].ToString() == "1")
             {
-                if (pos1 == true)
-                {
-                 Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, -1), Camera.transform.rotation);
-                    
-                    
-                }
-                if (pos2 == true)
-                {
-                    Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, 0), Camera.transform.rotation);
-                }
-                if (pos3 == true)
-                {
-                    Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, 1), Camera.transform.rotation);
-                }
-
-
-                else
-                {
-                    Debug.Log("No blocks to spawn");
-                }
+                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, -1), Camera.transform.rotation);
+                hasSpawned = true;
+            }
+            if (input[2].ToString() == "1")
+            {
+                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, 0), Camera.transform.rotation);
+                hasSpawned = true;
+            }
+            if (input[3].ToString() == "1")
+            {
+                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, 1), Camera.transform.rotation);
+                hasSpawned = true;
             }
 
+            if (hasSpawned == true)
+            {
+                Debug.Log("Can't spawn just yet.");
+            }
 
+            else
+            {
+                Debug.Log("No blocks to spawn");
+                hasSpawned = false;
+            }
+        }
+        buttonPressed = false;
     }
 
 
@@ -137,7 +97,6 @@ public class Spawner : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            //*ChangeDisplay();*//*
             arrayPos += 1;
             Debug.Log("Changing block.");
             if (arrayPos >= blocks.Length)
@@ -148,133 +107,4 @@ public class Spawner : MonoBehaviour
             }
         }
     }
-        //Read the set tag to dertermine the array to be spawned from.
-
-        //In all permeations, right click sets player into spawn mode
-        //Tab cycles through the pieces in the array
-       /* //Left click to spawn object.
-        if (gameController.CompareTag("Normal"))
-        {
-            
-            Debug.Log("Set to Normal");
-            //Sets the player to spawn mode
-            if (Input.GetMouseButton(1))
-            {
-*//*                if (isDisplayActive == false)
-                {
-                    SpawnDisplay();
-                    *//*isDisplayActive = true;*//*
-                }
-                else if (isDisplayActive == true)
-                {
-                    
-                }*//*
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Instantiate(normalBlocks[arrayPos]);
-                }
-
-                //Cycles through the blocks in the given array
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    *//*ChangeDisplay();*//*
-                    arrayPos += 1;
-                    Debug.Log("Changing ice block.");
-                    if (arrayPos >= normalBlocks.Length)
-                    {
-                        //Array resets and loops
-                        arrayPos = 0;
-                        Debug.Log("Resetting array.");
-                    }
-                }
-            }
-        }
-
-        if (gameController.CompareTag("Icy"))
-        {
-
-            Debug.Log("Set to Icy");
-            //Sets the player to spawn mode
-            if (Input.GetMouseButton(1))
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Instantiate(iceBlocks[arrayPos]);
-                }
-
-                //Cycles through the blocks in the given array
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    arrayPos += 1;
-                    Debug.Log("Changing ice block.");
-                    if (arrayPos >= iceBlocks.Length)
-                    {
-                        //Array resets and loops
-                        arrayPos = 0;
-                        Debug.Log("Resetting array.");
-                    }
-                }
-            }
-        }
-
-        if (gameController.CompareTag("Bouncy"))
-        {
-
-            Debug.Log("Set to Bouncy");
-
-            //Sets the player to spawn mode
-            if (Input.GetMouseButton(1))
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Instantiate(bounceBlocks[arrayPos]);
-                }
-
-                //Cycles through the blocks in the given array
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    arrayPos += 1;
-                    Debug.Log("Changing ice block.");
-                    if (arrayPos >= bounceBlocks.Length)
-                    {
-                        //Array resets and loops
-                        arrayPos = 0;
-                        Debug.Log("Resetting array.");
-                    }
-                }
-
-
-            }
-        }
-    }*/
-
-/*    void SpawnDisplay()
-    {
-        GameObject display = GameObject.Instantiate(normalBlocks[arrayPos], new Vector3(0, 2000, 100), Quaternion.identity);
-        display.GetComponent<Rigidbody>().isKinematic = true;
-
-
-        Instantiate(displayCamera, new Vector3(0, 2000, 80), Quaternion.identity);
-
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            Destroy(display);
-            arrayPos += 1;
-            if (arrayPos >= bounceBlocks.Length)
-            {
-                //Array resets and loops
-                arrayPos = 0;
-            }
-                SpawnDisplay();
-        }
-    }*/
-
-
-/*    void ChangeDisplay()
-    {
-        GameObject display = SpawnDisplay(display);
-    }*/
-
 }
-
