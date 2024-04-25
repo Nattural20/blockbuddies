e36bfb5ee5ca94d0c7ce473548c87d80ae0a4bcd
EarlM001 <143171813+EarlM001@users.noreply.github.com>
2024-04-25 20:05:45 +1000

    Updated Spawner.cs diagnonal point bigfix
    Fixed an issue where blocks jointed on more then one axis would freak out.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,8 +10,8 @@ using UnityEditor.Search;
 public class Spawner : MonoBehaviour
 {
 
-    public Camera Camera;
-
+    //public Camera Camera;
+    public GameObject SpawnPosGuide;
 
     public GameObject[] blocks;
 
@@ -32,10 +32,10 @@ public class Spawner : MonoBehaviour
     {   
         CycleBlocks();
         DisplayPos();
-        Spawn();    
-  
-    }
+        Spawn();
+        //ModularSpawn(); //new spawn method that utilises a foreach loop, can accept as many inputs as neccessary
 
+    }
 
     void DisplayPos() 
     {
@@ -64,48 +64,48 @@ public class Spawner : MonoBehaviour
             buttonPressed = true;
             if (input[1].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, -1), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(4, 1, -1), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             if (input[2].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, 0), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(4, 1, 0), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             if (input[3].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(4, 1, 1), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(4, 1, 1), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             //temporary hardcode 3x3 grid until foreach is working
             if (input[4].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(5, 1, -1), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(5, 1, -1), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             if (input[5].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(5, 1, 0), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(5, 1, 0), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             if (input[6].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(5, 1, 1), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(5, 1, 1), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             if (input[7].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(6, 1, -1), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(6, 1, -1), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             if (input[8].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(6, 1, 0), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(6, 1, 0), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
             if (input[9].ToString() == "1")
             {
-                Instantiate(blocks[arrayPos], Camera.transform.position + new Vector3(6, 1, 1), Camera.transform.rotation);
+                Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(6, 1, 1), new Quaternion(0, SpawnPosGuide.transform.rotation.y, 0, 0));
                 hasSpawned = true;
             }
 
@@ -126,6 +126,26 @@ public class Spawner : MonoBehaviour
             buttonPressed = false;
         } 
         
+    }
+
+    private void ModularSpawn() //WIP: Do not use this method. 
+    {
+        char[] arduinoInput = GetComponent<ArduinoReader>().OutputArray;
+        if (arduinoInput[0].ToString() == "1" && buttonPressed == false)
+        {
+            foreach (char c in arduinoInput)
+            {
+                if (c.ToString() == "1")
+                {
+                    hasSpawned = true;
+                    Instantiate(blocks[arrayPos], SpawnPosGuide.transform.position + new Vector3(6, 1, 1), new Quaternion(0, SpawnPosGuide.transform.rotation.y, SpawnPosGuide.transform.rotation.z, 0));
+                }
+            }
+        }
+        else if (buttonPressed == true && arduinoInput[0].ToString() == "0") //reset buttonpressed if no input is detected
+        {
+            buttonPressed = false;
+        }
     }
 
 
