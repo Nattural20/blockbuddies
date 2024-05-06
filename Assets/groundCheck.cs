82d84d69dd524285f4a0ad6bde3214e5cb077c3c
jberry2410 <jakeberry2410@gmail.com>
2024-05-06 11:18:21 +1000

    progress on new jump in jakes scene

---
+++ Assets/groundCheck.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class groundCheck : MonoBehaviour
+{
+    public PlayerController pC;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            pC.isGrounded = true;
+        }
+    }
+}
