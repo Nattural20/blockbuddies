d0143482877fe25ed215568f11ac87c924e8b87c
jberry2410 <jakeberry2410@gmail.com>
2024-05-18 00:25:41 +1000

    vineelevator progress

--- Assets/vineElevator.cs
+++ Assets/vineElevator.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class vineElevator : MonoBehaviour
 {
     public GameObject riseToThisPoint, fallToThisPoint, topOfObject;
+    public GameObject elevator;
+    public Collider vineElevatorCollider;
 
     public LavaScript lS;
-
     private List<GameObject> movingGroup;
+
+    public bool rising;
+    public float elevatorSpeed = 5;
 
 
     // Start is called before the first frame update
@@ -21,11 +25,37 @@ public class vineElevator : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        foreach (var obj in movingGroup)
-        {
-
-            //obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y - (sinkSpeed * Time.deltaTime), obj.transform.position.z);
-        }
+        //foreach (var obj in movingGroup)
+        //{
+            if (rising)
+            {
+                elevator.transform.Translate(0, elevatorSpeed * Time.fixedDeltaTime, 0);
+            }
+            else
+            {
+                elevator.transform.Translate(0, -elevatorSpeed * Time.fixedDeltaTime, 0);
+            }
+        //}
         
+    }
+
+    private void Update()
+    {
+        if (rising && topOfObject.transform.position.y > riseToThisPoint.transform.position.y)
+        {
+            rising = false;
+        }
+        else if (!rising && topOfObject.transform.position.y < fallToThisPoint.transform.position.y)
+        {
+            rising = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Body")
+        {
+            //rising = true;
+        }
     }
 }
