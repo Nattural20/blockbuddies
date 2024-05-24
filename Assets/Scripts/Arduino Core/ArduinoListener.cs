49c2fc205529a18c719bea4ad385b801059f614f
EarlM001 <143171813+EarlM001@users.noreply.github.com>
2024-05-24 20:33:19 +1000

    WIP Arduino Listener script
    To be called whenever any Arduino script needs to assign a string to "comPort".

---
+++ Assets/Scripts/Arduino Core/ArduinoListener.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class ArduinoListener : MonoBehaviour
+{
+    public string comPortReal;
+    public string[] _possibleComPorts = {"2",  "3", "4", "5", "6"};
+    static SerialPort serialPort;
+    static int baudRate = 9600;
+    /// <summary>
+    /// WIP Arduino Listener. 
+    /// </summary>
+
+    public string FindComPort()
+    {///Family Guy Funny Moment Compilations #23
+        //go through and try every port, see what returns something
+        foreach (string port in _possibleComPorts)
+        {
+            serialPort = new SerialPort(port, baudRate);
+            try
+            {
+                serialPort.Open();
+                serialPort.ReadTimeout = 50; // Adjust as necessary
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return ("No Port Found on: " + port);
+            }
+            if (serialPort != null)
+            {
+                comPortReal = port;
+                return port;
+            }
+        }
+        return ("Reached end of method. No Port Found.");
+    }
+}
