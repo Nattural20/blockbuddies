ab972350c181409234bbe7bb98ddd08f3d484090
EleUehara <103998768@student.swin.edu.au>
2024-05-26 23:12:44 +1000

    Update StartScreenCameraMove.cs

--- Assets/Scripts/UI/StartScreenCameraMove.cs
+++ Assets/Scripts/UI/StartScreenCameraMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StartScreenCameraMove : MonoBehaviour
 {
     public GameObject playerCam, startCam;
+    public SpawnerSpoofUnspoof spawner;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Body"))
@@ -12,5 +13,10 @@ public class StartScreenCameraMove : MonoBehaviour
             startCam.SetActive(false);
             playerCam.SetActive(true);
         }
+    }
+    IEnumerator TurnOnSpocks()
+    {
+        yield return new WaitForSeconds(1);
+        spawner.canSpawnSpocks = true;
     }
 }
