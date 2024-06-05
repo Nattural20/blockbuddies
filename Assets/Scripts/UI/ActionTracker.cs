a89277c2261154dfa798a25767b39f9f8a21cccc
jberry2410 <jakeberry2410@gmail.com>
2024-06-05 22:27:46 +1000

    removed un-used animation line that caused build erorrs

--- Assets/Scripts/UI/ActionTracker.cs
+++ Assets/Scripts/UI/ActionTracker.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class ActionTracker : MonoBehaviour
