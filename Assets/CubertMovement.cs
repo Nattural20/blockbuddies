3390fd0015f54c95f4e7efc74a69335d00e3e470
jberry2410 <jakeberry2410@gmail.com>
2024-09-22 23:29:29 +1000

    cubert looks in direction of movement

--- Assets/CubertMovement.cs
+++ Assets/CubertMovement.cs
@@ -10,15 +10,15 @@ public class CubertMovement : MonoBehaviour
     {
         if (CubertAimPosition != null)
         {
+            Vector3 direction = (CubertAimPosition.position - transform.position).normalized;
             Vector3 newPosition = Vector3.Lerp(transform.position, CubertAimPosition.position, moveSpeed * Time.deltaTime);
             transform.position = newPosition;
 
-            Quaternion targetRotation = CubertAimPosition.rotation;
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-            Vector3 eulerAngles = transform.rotation.eulerAngles;
-            eulerAngles.z = 0;
-            transform.rotation = Quaternion.Euler(eulerAngles);
+            if (direction.magnitude > 0.01f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
