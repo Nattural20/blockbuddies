b12c394c2e31d7ad9de74372d0d4a520a913124a
jberry2410 <jakeberry2410@gmail.com>
2024-09-22 23:36:56 +1000

    animated thorn explosion thingo

--- Assets/Scripts/Puzzle/Lava Geyser/ThornTimed.cs
+++ Assets/Scripts/Puzzle/Lava Geyser/ThornTimed.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Remoting;
 using UnityEngine;
 
 public class ThornTimed : MonoBehaviour
@@ -10,23 +8,30 @@ public class ThornTimed : MonoBehaviour
     private bool _isTriggered;
     public float thornsTimer = 3f;
 
-    private Vector3 setPosition, risePositon;
+    private Vector3 setPosition, risePosition;
+    private float riseSpeed = 10f;
+    private bool isRising = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         setPosition = transform.position;
-        risePositon = transform.position + new Vector3(0, 5, 0);
+        risePosition = transform.position + new Vector3(0, 5, 0);
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    void Update()
+    {
+        if (isRising)
+        {
+            transform.position = Vector3.Lerp(transform.position, risePosition, riseSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, risePosition) < 0.1f)
+            {
+                isRising = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider col)
-    {///Vocal cords Ready
+    {
         if (!_isTriggered)
         {
             if (col.CompareTag("Spocks") || col.CompareTag("Lava Spock"))
@@ -37,26 +42,17 @@ public class ThornTimed : MonoBehaviour
                 Debug.Log("1st trigger Triggered. " + _affectedSpock);
             }
         }
-
     }
 
     IEnumerator ThornExplode(GameObject spock)
-    { ///let the show Begin
-        //Debug.Log("trigger Triggered");
-        //yield return new WaitForSeconds(thornTimer);
-        //Debug.Log("Timer Done");
+    {
+        isRising = true;
 
-        transform.position = risePositon;
-
-        //check for sinking spocks
         if (spock.CompareTag("Lava Spock"))
         {
             spock.GetComponent<LavaSpockScript>().enabled = false;
-
             spock.GetComponent<Rigidbody>().isKinematic = false;
-
             spock.GetComponent<Rigidbody>().AddForce(new Vector3(0, thornForce, 0), ForceMode.Impulse);
-            //****NOTE: THIS Only effects one GameObject Spock at a time- in future, look towards adjusting to take in an array and affect all of them. 
         }
         else if (spock.CompareTag("Spocks"))
         {
@@ -66,7 +62,6 @@ public class ThornTimed : MonoBehaviour
         yield return new WaitForSeconds(thornsTimer);
         transform.position = setPosition;
         _isTriggered = false;
-
         spock.tag = "Spocks";
     }
 }
