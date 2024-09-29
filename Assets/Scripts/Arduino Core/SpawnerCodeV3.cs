7d9b8b591372f29028f237f778d8844d049d4c96
EarlM001 <143171813+EarlM001@users.noreply.github.com>
2024-09-29 15:10:45 +1000

    Added AutoAssignment Pause Script, SpawnerCodeV3 tweaks

--- Assets/Scripts/Arduino Core/SpawnerCodeV3.cs
+++ Assets/Scripts/Arduino Core/SpawnerCodeV3.cs
@@ -451,9 +451,31 @@ public class SpawnerCodeV3 : MonoBehaviour
         }
     }
 
+    public void ToggleSpoofBool(bool input)
+    {
+        // Check if the Toggle is on
+        if (input == true)
+        {
+            enableSpoof = true;
+            TogOn = true;
+            Debug.Log("Spoofing enabled.");
+        }
+        // If the Toggle is off and it was previously on, disable the spoofing
+        else if (input == false)
+        {
+            enableSpoof = false;
+            TogOn = false;
+            Debug.Log("Spoofing disabled.");
+        }
+    }
+
     public void TriggerHapticFeedback()
     {
-        StartCoroutine(HapticCoroutine());
+        // catch to stop error messages in console
+        if (Gamepad.current != null)
+        {
+            StartCoroutine(HapticCoroutine());
+        }
     }
 
     private IEnumerator HapticCoroutine()
