c7fde57fd5313865550317a14ec78e299f09623e
EleUehara <103998768@student.swin.edu.au>
2024-09-29 14:49:47 +1000

    Updated LilyPadLockZone prefab

---
+++ Assets/Scripts/Puzzle/Floating Lilypads/LilyPadSuckedDown.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using UnityEngine;
+
+public class LilyPadSuckedDown : MonoBehaviour
+{
+    public float suckSpeed;
+    bool suckedDown;
+    Vector3 floatPos, suckedPos;
+    private void Start()
+    {
+        floatPos = transform.localPosition;
+        suckedPos = floatPos + new Vector3(2, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        if (suckedDown)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, suckedPos, suckSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, floatPos, suckSpeed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Lily Pad Sucker"))
+        {
+            suckedDown = true;
+            StartCoroutine(ResetSuck());
+        }
+    }
+    IEnumerator ResetSuck()
+    {
+        yield return new WaitForSeconds(2);
+        suckedDown = false;
+    }
+}
