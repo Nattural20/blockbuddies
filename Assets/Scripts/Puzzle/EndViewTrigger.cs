de085cfd041765c7209e95f058f6e6fcc6a70175
EleUehara <103998768@student.swin.edu.au>
2024-10-05 18:25:11 +1000

    Added End Screen fade in. Added respawn planes

--- Assets/Scripts/Puzzle/EndViewTrigger.cs
+++ Assets/Scripts/Puzzle/EndViewTrigger.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndViewTrigger : MonoBehaviour
 {
     public LayerMask playerMask;
     public GameObject freeLookCam, endViewCam;
-
+    public Image endScreen;
+    public float endScreenFadeSpeed;
+    bool isFinished = false;
 
     private void OnTriggerEnter(Collider col)
     {
@@ -14,7 +17,16 @@ public class EndViewTrigger : MonoBehaviour
         {
             freeLookCam.SetActive(false);
             endViewCam.SetActive(true);
+
+            endScreen.gameObject.SetActive(true);
+            isFinished = true;
         }
     }
-
+    private void Update()
+    {
+        if (isFinished)
+        {
+            endScreen.color = new Color(endScreen.color.r, endScreen.color.g, endScreen.color.b, endScreen.color.a + endScreenFadeSpeed * Time.deltaTime);
+        }
+    }
 }
