209402c9923a67dadfd159708d4438f9e1fd3c0e
EarlM001 <143171813+EarlM001@users.noreply.github.com>
2024-10-08 23:52:30 +1100

    Water wading sounds for end pond area
    Move from still sounds change if you're in the final end pond

--- Assets/HopperMoveSounds.cs
+++ Assets/HopperMoveSounds.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,26 +6,36 @@ public class HopperMoveSounds : MonoBehaviour
 {
     public PlayerController hopper;
     private bool moveCueFired = false;
-    // Start is called before the first frame update
+    private bool inWater = false; 
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         CheckHopperMovement();
         CheckAnyMovement();
+        if (inWater)
+        {
+            Debug.Log("WATERWATERWATERWATER");
+        }
     }
 
     private void CheckHopperMovement()
     {
         if ((hopper.currentMovementDirection != Vector3.zero) && (moveCueFired == false))
         {
-            //hopper moved
-            //Debug.Log("Hopper has moved");
-            FindAnyObjectByType<AudioManager>().Play("MoveFromStill");
+            //Play different sound if in water
+            if (inWater)
+            {
+                FindAnyObjectByType<AudioManager>().Play("PlayerInWater");
+            }
+            else
+            {
+                FindAnyObjectByType<AudioManager>().Play("MoveFromStill");
+            }
+
             moveCueFired = true;
         }
     }
@@ -38,4 +47,23 @@ public class HopperMoveSounds : MonoBehaviour
             moveCueFired = false;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Body"))
+        {
+            inWater = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (other.CompareTag("Body"))
+        {
+            inWater = false; //REMOVE TAG when leaving water trigger
+        }
+    }
 }
+
