537483b314d7ed02f2de9d150a541abaed8906e5
EleUehara <103998768@student.swin.edu.au>
2024-10-09 18:36:35 +1100

    Camera focus change

--- Assets/Scripts/Player Controller/CameraFocusPull.cs
+++ Assets/Scripts/Player Controller/CameraFocusPull.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class CameraFocusPull : MonoBehaviour
 {
     public CinemachineFreeLook machine;
+    Vector2 defaultSpeed;
+
     CinemachineFreeLook newCam;
 
     public Transform newLookAt;
@@ -17,6 +19,9 @@ public class CameraFocusPull : MonoBehaviour
         if (other.gameObject.CompareTag("Body"))
         {
             GetComponent<Collider>().enabled = false;
+            defaultSpeed = new Vector2 (machine.m_XAxis.m_MaxSpeed, machine.m_YAxis.m_MaxSpeed);
+            machine.m_XAxis.m_MaxSpeed = 0;
+            machine.m_YAxis.m_MaxSpeed = 0;
 
             newCam = Instantiate(machine);
             machine.gameObject.SetActive(false);
@@ -25,6 +30,7 @@ public class CameraFocusPull : MonoBehaviour
 
             newCam.m_XAxis.m_MaxSpeed = 0;
             newCam.m_YAxis.m_MaxSpeed = 0;
+            newCam.m_YAxis.Value = 1;
             newCam.gameObject.GetComponent<CinemachineCollider>().enabled = false;
 
             StartCoroutine(StopLook());
@@ -35,6 +41,8 @@ public class CameraFocusPull : MonoBehaviour
         yield return new WaitForSeconds(lookLength);
         newCam.gameObject.SetActive(false);
         machine.gameObject.SetActive(true);
+        machine.m_XAxis.m_MaxSpeed = defaultSpeed.x;
+        machine.m_YAxis.m_MaxSpeed = defaultSpeed.y;
         yield return new WaitForSeconds(5);
         Destroy(newCam.gameObject);
         Destroy(this);
