8e645b7fbba5944040631c0c06666b31a5b98f99
EleUehara <103998768@student.swin.edu.au>
2024-10-09 22:44:46 +1100

    oops

--- Assets/Scripts/Puzzle/Player Help/PlayerHelpRiver.cs
+++ Assets/Scripts/Puzzle/Player Help/PlayerHelpRiver.cs
@@ -38,7 +38,7 @@ public class PlayerHelpRiver : PlayerHelperScripts
             {
                 if (resetD)
                 {
-                    if (Input.GetAxis("DualPadVertical") != 0) // DPad Up
+                    if (Input.GetAxis("DualPadVertical") > 0) // DPad Up
                     {
                         if (!helpFunctionOn)
                             SlowRiver();
